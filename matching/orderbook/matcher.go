5477e985351cd67f200467a622ec415ad04224b7
Saikat Das <sd.saikat396@gmail.com>
2025-11-05 22:31:14 +0530

    feat: Added Market Orders

--- matching/orderbook/matcher.go
+++ matching/orderbook/matcher.go
@@ -26,6 +26,14 @@ func (ob *OrderBook) canMatch(order *orders.MatchOrder) bool {
 }
 
 func (ob *OrderBook) TryMatch(order *orders.MatchOrder) {
+    if order.Type == "MARKET" {
+        ob.matchMarketOrder(order)
+    } else {
+        ob.matchLimitOrder(order)
+    }
+}
+
+func (ob *OrderBook) matchLimitOrder(order *orders.MatchOrder) {
     for ob.canMatch(order) {
         if order.Quantity == 0 {
             break
@@ -46,6 +54,40 @@ func (ob *OrderBook) TryMatch(order *orders.MatchOrder) {
             }
 
         case "SELL":
+            bestBuy := ob.BuyOrders.Peek()
+            tradeQty := min(order.Quantity, bestBuy.Quantity)
+
+            trade := trades.New(bestBuy, order, tradeQty, bestBuy.Price)
+            ob.TradeOut <- trade
+
+            order.Quantity -= tradeQty
+            bestBuy.Quantity -= tradeQty
+            if bestBuy.Quantity == 0 {
+                ob.BuyOrders.Pop()
+            }
+        }
+    }
+}
+
+func (ob *OrderBook) matchMarketOrder(order *orders.MatchOrder) {
+    switch order.Side {
+    case "BUY":
+        for order.Quantity > 0 && ob.SellOrders.Len() > 0 {
+            bestSell := ob.SellOrders.Peek()
+            tradeQty := min(order.Quantity, bestSell.Quantity)
+
+            trade := trades.New(bestSell, order, tradeQty, bestSell.Price)
+            ob.TradeOut <- trade
+
+            order.Quantity -= tradeQty
+            bestSell.Quantity -= tradeQty
+            if bestSell.Quantity == 0 {
+                ob.SellOrders.Pop()
+            }
+        }
+
+    case "SELL":
+        for order.Quantity > 0 && ob.BuyOrders.Len() > 0 {
             bestBuy := ob.BuyOrders.Peek()
             tradeQty := min(order.Quantity, bestBuy.Quantity)
 
